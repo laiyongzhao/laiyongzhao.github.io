7ab3aedc710aaa6ac37a169571d796dccf069394
laiyongzhao <646149636@qq.com>
2024-09-12 14:09:36 +0800

    更改网站地址

--- _next/static/chunks/app/page-bff99c63b5674a68.js
+++ _next/static/chunks/app/page-bff99c63b5674a68.js
@@ -1383,7 +1383,7 @@
                             ]
                         }), (0, a.jsxs)("p", {
                             className: "dark:text-white text-slate-900 tracking-wide leading-6 text-xs sm:text-sm",
-                            children: ["珍惜当下", (0, a.jsx)("br", {}), "微信号：lailailai10",(0, a.jsx)("br", {}), "SEEK官方网站：http://laiyongzhao.com/SEEK"]
+                            children: ["珍惜当下", (0, a.jsx)("br", {}), "微信号：lailailai10",(0, a.jsx)("br", {}), "SEEK官方网站：https://laiyongzhao.com/SEEK"]
                         })]
                     })
                 });
